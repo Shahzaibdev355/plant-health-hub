fd507a3cd659f7024875690687fdbdaeba4ec00b
Shahzaib <shahzaibsheikh355@gmail.com>
2026-02-05 19:34:18 +0500

    route changed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1", // change if needed
-  // baseURL: "https://two-factor-authentication-two.vercel.app/api/v1",
+  // baseURL: "http://localhost:5000/api/v1", // change if needed
+  baseURL: "https://two-factor-authentication-two.vercel.app/api/v1",
   withCredentials: true,
 });
 
